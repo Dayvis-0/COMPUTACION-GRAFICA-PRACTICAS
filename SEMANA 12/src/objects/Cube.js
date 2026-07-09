f2cf8d075097851a57561e3c397cfb31232adacb
Dayvis <amdayvis39@gmail.com>
2026-07-09 15:53:24 -0500

    09/07/2026

---
+++ SEMANA 12/src/objects/Cube.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+import { loadTexture } from '../core/AssetLoader.js';
+
+export function createCube() {
+    const geometry = new THREE.BoxGeometry(1.5, 1.5, 1.5);
+    const texture = loadTexture('./src/textures/wood.png');
+    const material = new THREE.MeshPhongMaterial({
+        //color: 0x1ED611,
+        map: texture,
+        shininess: 120,
+        specular: 0xffffff
+    });
+
+    const cube = new THREE.Mesh(geometry, material);
+    cube.castShadow = true;
+
+    return cube;
+}
